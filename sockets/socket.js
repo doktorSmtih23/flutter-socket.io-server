340a77c1043eb3037d2db72094602ca87b6b273e
Christian Ordonez <csordonez@hotmail.com>
2020-11-02 20:20:03 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,22 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('ColdPlay'));
+bands.addBand(new Band('FooFighters'));
+bands.addBand(new Band('Led Zepellin'));
+
+console.log(bands);
+
 
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
 
     client.on('disconnect', () => { console.log('Cliente desconectado'); });
@@ -10,7 +24,27 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
-
     });
+
+    client.on('emitir-mensaje', (payload) => {
+        client.broadcast.emit('nuevo-mensaje', payload);
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
 
 });
